e03d2c5de02990cd9149af5fccd92c1c59ab29fa
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2021-12-06 20:55:57 +0700

    Update ashdgaysdgae.sh

--- mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
+++ mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
@@ -1,2 +1,2 @@
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o buatid -w #Build source code into executable
-echo -e "TOOL SUKSES SAATNYA KETIK ./buatid"| boxes -d parchment -a c
+echo -e "TOOL SUKSES SAATNYA KETIK ./buatid"
