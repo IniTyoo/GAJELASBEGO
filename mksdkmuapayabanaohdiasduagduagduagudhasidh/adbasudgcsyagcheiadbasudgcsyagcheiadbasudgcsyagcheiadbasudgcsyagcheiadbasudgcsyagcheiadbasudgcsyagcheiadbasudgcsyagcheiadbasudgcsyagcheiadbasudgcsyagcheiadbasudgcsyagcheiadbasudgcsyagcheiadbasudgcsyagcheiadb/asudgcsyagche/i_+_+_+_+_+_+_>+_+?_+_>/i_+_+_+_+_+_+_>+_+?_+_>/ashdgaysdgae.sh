7315a1019c2015f3c01aa38a3df5419e98bfcb81
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2021-12-06 21:17:49 +0700

    Update ashdgaysdgae.sh

--- mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
+++ mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
@@ -1,3 +1,9 @@
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o buatid -w #Build source code into executable
+echo -e "-"
+echo -e "-"
+echo -e "-"
+echo -e "-"
+echo -e "-"
 echo -e "TOOL SUKSES SAATNYA KETIK ./buatid"
+echo -e "-"
 echo -e "Group ? https://chat.whatsapp.com/DpHi1DoTBr27SbB4Gq3EY9"
