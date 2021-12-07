e5e77369b85f57ff49176c3d958ddcb788ac5427
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2021-12-07 10:40:47 +0700

    Update asdasdasdasdsaddgae.sh

--- mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/asdasdasdasdsaddgae.sh
+++ mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/asdasdasdasdsaddgae.sh
@@ -1,6 +1,4 @@
-
-clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o buatid -w #Build source code into executable
-echo ""
+clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -pthread -std=c++11 -fpermissive -o createid -w #Build source code into executableecho ""
 echo "================================="
 echo "⚠This Script Remaked By Mymet Code⚠"
 echo ""
