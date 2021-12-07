f31f7980339d47ee046440ab06c1f8b6af850603
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2021-12-07 10:34:37 +0700

    Update ashdgaysdgae.sh

--- mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
+++ mksdkmuapayabanaohdiasduagduagduagudhasidh/adbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadbasudgcsyagcheiadb/asudgcsyagche/i_+_+_+_+_+_+_>+_+?_+_>/i_+_+_+_+_+_+_>+_+?_+_>/ashdgaysdgae.sh
@@ -1,4 +1,5 @@
 # If you already install g++ or build-essential, use this
+sudo apt-get install clang
 clang++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o buatid -w #Build source code into executable
 echo ""
 echo "================================="
